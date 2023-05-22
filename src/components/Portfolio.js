451d0551654683fed46daf78a8d8c9f2f4d22536
whitneywishart <whitneywishart@gmail.com>
2023-05-22 16:21:49 -0700

    populate portfolio page

--- src/components/Portfolio.js
+++ src/components/Portfolio.js
@@ -3,8 +3,65 @@ import React from 'react';
 const Portfolio = () => {
   return (
     <div>
-      Portfolio content
-    </div>
+      <div>
+        <h2 className="left-rail">Work</h2>
+      </div>
+      <div className="card-box">
+        <div className="card-box-item">
+          <h3>Cucumber or Pickle</h3>
+          <h3><small>RESTful API</small></h3>
+          <p><a href="https://github.com/anryl050/cucumber-or-pickle" target="_blank">View repo</a> | <a
+
+            href="https://cucumber-or-pickle.herokuapp.com/" target="_blank">View Heroku deployment</a></p>
+
+          <a href="https://cucumber-or-pickle.herokuapp.com/" target="_blank">
+            <div><img src="/cucumber.png" /></div>
+          </a>
+
+        </div>
+        <div className="card-box-item">
+
+          <h3>Lyrics Legend</h3>
+          <h3><small>Server-side API</small></h3>
+          <p><a href="https://github.com/whitneywishart/project-one-lyrics-legend" target="_blank">View repo</a> | <a href="https://whitneywishart.github.io/project-one-lyrics-legend/" target="_blank">View GitHub deployment</a></p>
+          <a href="https://whitneywishart.github.io/project-one-lyrics-legend/" target="_blank">
+            <div><img src="/lyrics.png" /></div>
+          </a>
+
+        </div>
+
+        <div className="card-box-item">
+          <a href="#" target="_blank">
+            <h3>Application three</h3>
+            <p>In development</p>
+            <div><img src="/fpo-screenshot.png" alt="Application placeholder" /></div>
+          </a>
+        </div>
+        <div className="card-box-item">
+          <a href="#" target="_blank">
+            <h3>Application four</h3>
+            <p>In development</p>
+            <div><img src="/fpo-screenshot.png" alt="Application placeholder" /></div>
+          </a>
+        </div>
+        <div className="card-box-item">
+          <a href="#" target="_blank">
+            <h3>Application five</h3>
+            <p>In development</p>
+            <div><img src="/fpo-screenshot.png" alt="Application placeholder" /></div>
+          </a>
+        </div>
+        <div className="card-box-item">
+          <a href="#" target="_blank">
+            <h3>Application six</h3>
+            <p>In development</p>
+            <div><img src="/fpo-screenshot.png" alt="Application placeholder" /></div>
+          </a>
+        </div>
+      </div>
+    </div >
+
+
   )
 }
 
