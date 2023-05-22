6c04f46860b4b5d18b61764b30fb6a555bf574e8
whitneywishart <whitneywishart@gmail.com>
2023-05-22 15:18:13 -0700

    add about me styles

--- src/components/AboutMe.js
+++ src/components/AboutMe.js
@@ -4,52 +4,88 @@ const AboutMe = () => {
   return (
     <div>
       <h2 className='left-rail'>About me</h2>
-      <div>
+      <div className='about'>
 
         <p>
-          Northwest native, full-stack web developer and digital content management generalist for high-visibility enterprise sites and digital marketing programs.
+          Northwest native, full-stack web developer and digital content management generalist for high-visibility enterprise sites and technical digital marketing programs.
         </p>
+
+        <h3>Full-stack web developer</h3>
+        <p> HTML, CSS, JavaScript, APIs/REST/JSON, MERN (MongoDB, Express.js, React.js, Node.js), MySQL, Heroku, Git</p>
+
+        <h3>Agile methodologies</h3>
         <p>
-          <strong>Full stack web development</strong>  |  HTML, CSS, JavaScript, APIs/REST/JSON, MERN (MongoDB, Express.js, React.js, Node.js), MySQL, Heroku, Git
+          Jira project administrator, including advanced JQL queries, enterprise level project and task setup and management (KEY level and execution level), status tracking and dynamically updating boards/wallboards
         </p>
+
+        <h3>Accessibility</h3>
         <p>
-          <strong>Agile methodologies</strong>  |  Jira project administrator, including advanced JQL queries, enterprise level project and task setup and management (KEY level and execution level), status tracking and dynamically updating boards/wallboards
+          Extensive knowledge of AA conformance requirements for web and PDF
         </p>
+
+        <h3>AEM</h3>
         <p>
-          <strong>Accessibility</strong>  |  Extensive knowledge of AA conformance requirements for web and PDF
+          Authoring power user, including template creation, content and experience fragment use, bulk updating, query builder, bulk updater and native reporting
         </p>
+
+        <h3>Adobe Creative Suite proficiency</h3>
+        <h4>Acrobat Pro</h4>
+        <p>PDF creation and accessibility optimization, native tools, custom tools</p>
+
+        <h4>Photoshop</h4>
+        <p>Advanced color correction, image editing, and optimization for web</p>
+
+        <h4>Illustrator</h4>
+        <p>Proficient illustration editing</p>
+
+        <h4>InDesign</h4>
+        <p>Advanced document editing including interactive PDF and branded styles creation</p>
+
+        <h4>Premiere</h4>
+        <p>Proficient video editing</p>
+
+        <h4>After Effects</h4>
+        <p>Proficient animation and graphic effects editing</p>
+
+        <h4>XD/Figma/Sketch</h4>
+        <p>Specification editing and pixel-perfect build execution</p>
+
+        <h3>Full MS Office Suite</h3>
         <p>
-          <strong>AEM</strong>  |  Authoring power user, including template creation, content and experience fragment use, bulk updating, query builder, bulk updater and native reporting
+          Advanced user for all apps, including advanced Excel, for work with complex formulas, pivot charts and tables, power query, data modeling, UDF, and VBA scripting for task automation (e.g. building a fully styled PowerPoint deck based on multiple Excel charts with one click)
         </p>
+        <h3>PowerShell proficiency</h3>
         <p>
-          <strong>Adobe Creative Suite proficiency</strong>  |  Acrobat Pro, PDF creation and accessibility optimization, native tools  |  Photoshop, advanced color correction, image editing, and optimization for web  |  Illustrator, advanced illustration editing  |  InDesign, advanced document editing including interactive PDF and branded styles creation  |  Premiere, proficient video editing  |  After Effects, proficient animation and effects editing  |  XD, specification editing and pixel-perfect build execution based on XD/Figma/Sketch</p>
+          Scripting for automated web scraping and efficient task execution
+        </p>
+
+        <h3>Enterprise content management for web</h3>
+        <p>
+          Best practices, process author, documentation, consistently styled and branded web authoring for vast multi-language sites, and localization management
+        </p>
+
+        <h3>UAT</h3>
+        <p>
+          Design and development collaboration to deliver above and beyond leadership expectations the first time, on time
+        </p>
+
+        <h3>A/B testing</h3>
+        <p>
+          Building Optimizely tests and developing recommendations for increased demand generation
+        </p>
+        <h3>Enterprise asset and DAM management</h3>
+        <p>
+        </p>
+        <h3>Pixel-perfect attention to detail with efficiency always at the forefront</h3>
+        <p>
+        </p>
+
+        <h3>Complete comfort with ambiguity, broad thinking self-sufficient troubleshooter</h3>
 
         <p>
-          <strong>Full MS Office Suite</strong>  |  Advanced user for all apps, including advanced Excel, for work with complex formulas, pivot charts and tables, power query, data modeling, UDF, and VBA scripting for task automation (e.g. building a fully styled PowerPoint deck based on multiple Excel charts with one click)
-        </p>
-        <p>
-          <strong>PowerShell proficiency</strong>  |  Scripting for automated web scraping and efficient task execution
-        </p>
-        <p>
-          <strong>Enterprise content management for web</strong>  |  Best practices, process author, documentation, consistently styled and branded web authoring for vast multi-language sites, and localization management
-        </p>
-        <p>
-          <strong>UAT</strong>  |  Design and development collaboration to deliver above and beyond leadership expectations the first time, on time
-        </p>
-        <p>
-          <strong>A/B testing</strong>  |  Building Optimizely tests and developing recommendations for increased demand generation
-        </p>
-        <p>
-          <strong>Enterprise asset and DAM management</strong>
-        </p>
-        <p>
-          <strong>Pixel-perfect attention to detail with efficiency always at the forefront</strong>
-        </p>
-        <p>
-          <strong>Complete comfort with ambiguity, broad thinking self-sufficient troubleshooter</strong>
         </p>
       </div>
-    </div>
+    </div >
   );
 }
 
